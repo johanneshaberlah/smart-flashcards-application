8fd9f65a9d5f468eeddd5db530c25978e87dbf4b
Johannes Haberlah <haberlah.j@zdf.de>
2024-06-02 11:57:40 +0200

    Add ...

--- src/views/stack/CreateOrUpdateCard.vue
+++ src/views/stack/CreateOrUpdateCard.vue
@@ -54,7 +54,7 @@
         <div v-if="existingCard" @click="visitCardCreation" class="mt-4 flex flex-shrink-0 md:ml-4 md:mt-0">
           <button @click="deleteCard" type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Löschen</button>
           <button type="button"
-                  class="lg:ml-3 inline-flex items-center rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
+                  class="ml-4 lg:ml-3 inline-flex items-center rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
             Neue Karte erstellen
           </button>
         </div>
