8d61dd760cd28ba6e81699fe591e1b3f564d7279
Johannes Haberlah <haberlah.j@zdf.de>
2024-06-02 12:18:42 +0200

    Add metadata

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -5,7 +5,7 @@
       <div  class="flex lg:flex-1">
         <a href="/dashboard" class="-m-1.5 p-1.5">
           <span class="sr-only">Smart-Flashcards</span>
-          <img class="h-8 w-auto" src="../assets/icons/brain.svg" alt="">
+          <img class="h-8 w-auto" src="../../public/brain.svg" alt="">
         </a>
       </div>
 
--- src/views/auth/LoginRegister.vue
+++ src/views/auth/LoginRegister.vue
@@ -100,7 +100,7 @@ const handleSubmit = async (e: Event) => {
       <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg]  bg-gradient-to-tr from-[#74eb34] to-[#079669] opacity-15 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
     </div>
     <div @click="router.push({ path: '/' })" class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-20 w-auto" src="../../assets/icons/brain.svg" alt="Smart Flashcards logo"/>
+      <img class="mx-auto h-20 w-auto" src="../../../public/brain.svg" alt="Smart Flashcards logo"/>
       <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         {{ props.action === 'login' ? 'Anmelden' : 'Registrieren' }}
       </h2>
